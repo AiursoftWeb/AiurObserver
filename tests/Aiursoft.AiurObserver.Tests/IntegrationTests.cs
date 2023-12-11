cbdf71ff48c9136e22e028309d7e6490de61a26f
AnduinXue <anduin@aiursoft.com>
2023-12-11 11:41:29 +0000

    Higher coverage.

--- tests/Aiursoft.AiurObserver.Tests/IntegrationTests.cs
+++ tests/Aiursoft.AiurObserver.Tests/IntegrationTests.cs
@@ -6,7 +6,48 @@ namespace Aiursoft.AiurObserver.Tests;
 public class IntegrationTests
 {
     [TestMethod]
-    public async Task DoTest()
+    public async Task BroadcastListenTest()
+    {
+        var totalMessages = 0;
+        var asyncObservable = new AsyncObservable<int>();
+        asyncObservable.Subscribe(_ =>
+        {
+            totalMessages++;
+            return Task.CompletedTask;
+        });
+        for (var i = 0; i < 10; i++)
+        {
+            await asyncObservable.BroadcastAsync(2333);
+        }
+
+        Assert.AreEqual(10, totalMessages);
+    }
+    
+    [TestMethod]
+    public async Task MultipleBroadcastListenTest()
+    {
+        var totalMessages = 0;
+        var asyncObservable = new AsyncObservable<int>();
+        asyncObservable.Subscribe(_ =>
+        {
+            totalMessages++;
+            return Task.CompletedTask;
+        });
+        asyncObservable.Subscribe(_ =>
+        {
+            totalMessages++;
+            return Task.CompletedTask;
+        });
+        for (var i = 0; i < 10; i++)
+        {
+            await asyncObservable.BroadcastAsync(2333);
+        }
+
+        Assert.AreEqual(20, totalMessages);
+    }
+    
+    [TestMethod]
+    public async Task UnRegisterTest()
     {
         var totalMessages = 0;
         var asyncObservable = new AsyncObservable<int>();
@@ -28,5 +69,71 @@ public class IntegrationTests
             await asyncObservable.BroadcastAsync(2333);
         }
         Assert.AreEqual(10, totalMessages);
+    }
+
+    [TestMethod]
+    public async Task UnRegisterMultiTimesFailedTest()
+    {
+        var asyncObservable = new AsyncObservable<int>();
+        var subscription = asyncObservable.Subscribe(_ => Task.CompletedTask);
+        subscription.UnRegister();
+        try
+        {
+            subscription.UnRegister();
+            Assert.Fail();
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+    }
+    
+    [TestMethod]
+    public async Task MultipleUnRegisterTest()
+    {
+        var totalMessages = 0;
+        var totalMessages2 = 0;
+
+        var asyncObservable = new AsyncObservable<int>(); 
+        asyncObservable.Subscribe(_ =>
+        {
+            totalMessages++;
+            return Task.CompletedTask;
+        });
+        var subscription2 = asyncObservable.Subscribe(_ =>
+        {
+            totalMessages2++;
+            return Task.CompletedTask;
+        });
+        for (var i = 0; i < 10; i++)
+        {
+            await asyncObservable.BroadcastAsync(2333);
+        }
+
+        Assert.AreEqual(10, totalMessages);
+        Assert.AreEqual(10, totalMessages2);
+
+        subscription2.UnRegister();
+        
+        for (var i = 0; i < 20; i++)
+        {
+            await asyncObservable.BroadcastAsync(2333);
+        }
+        Assert.AreEqual(30, totalMessages);
+        Assert.AreEqual(10, totalMessages2);
+    }
+
+    [TestMethod]
+    public void GetListenerCountTest()
+    {
+        var asyncObservable = new AsyncObservable<int>();
+        var sub1 = asyncObservable.Subscribe(_ => Task.CompletedTask);
+        var sub2 = asyncObservable.Subscribe(_ => Task.CompletedTask);
+
+        Assert.AreEqual(2, asyncObservable.GetListenerCount());
+        sub2.UnRegister();
+        Assert.AreEqual(1, asyncObservable.GetListenerCount());
+        sub1.UnRegister();
+        Assert.AreEqual(0, asyncObservable.GetListenerCount());
     }
 }
