9a4883bb569db13b995d6b02827f02c9f48232c8
Anduin Xue <xuef@microsoft.com>
2024-01-05 13:09:54 +0000

    Move files.

--- src/Aiursoft.AiurObserver.Extensions/DefaultConsumers/MessageStageSpecific.cs
+++ src/Aiursoft.AiurObserver.Extensions/DefaultConsumers/MessageStageSpecific.cs
@@ -1,17 +1,26 @@
 namespace Aiursoft.AiurObserver;
 
-public class MessageStageFirst<T> : AsyncObservable<T>, IConsumer<T>
+public class MessageStageSpecific<T>: AsyncObservable<T>, IConsumer<T>
 {
+    private readonly int _index;
+    private int _current;
     public bool IsStaged { get; private set; }
 
     public T? Stage { get; private set; }
+    
+    public MessageStageSpecific(int index)
+    {
+        _index = index;
+    }
+
     public async Task Consume(T value)
     {
-        if (!IsStaged)
+        if (_current == _index)
         {
             Stage = value;
             IsStaged = true;
             await BroadcastAsync(value);
         }
+        _current++;
     }
 }
